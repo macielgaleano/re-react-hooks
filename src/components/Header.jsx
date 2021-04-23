11bd44c72dc1490769265e9117eba2d198db204d
Maciel Galeano <macielgaleano.jh@gmail.com>
2021-04-23 17:35:15 -0300

    Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,17 @@ import React from "react";
 export default function Header({ darkMode, handleClick }) {
   return (
     <div className={`Header ${darkMode ? "header-darkmode" : "header-ligth"}`}>
-      <h1>RE-React hooks</h1>
-      <div>
-        <button
-          className={darkMode ? "button-darkmode" : "button-ligth"}
-          onClick={handleClick}
-          type="button"
-        >
-          {darkMode ? "Darkmode" : "Light"}
-        </button>
+      <div className="centerHeader">
+        <h1>RE-React hooks</h1>
+        <div>
+          <button
+            className={darkMode ? "button-darkmode" : "button-ligth"}
+            onClick={handleClick}
+            type="button"
+          >
+            {darkMode ? "Darkmode" : "Light"}
+          </button>
+        </div>
       </div>
     </div>
   );
