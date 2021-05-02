0b314512ca7a7c23fce8627ba3c9c4a889175052
Maciel Galeano <macielgaleano.jh@gmail.com>
2021-05-02 13:11:55 -0300

    Esta noche me desacato

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-export default function Character({ character, darkMode }) {
+export default function Character({ character, darkMode, handleClick, favorites }) {
   return (
     <div className={`character ${darkMode ? "character-darkmode" : "character-ligth"}`}>
-      <h3>{character.name}</h3>
+      <h3>{character.name} </h3>
       <h4 className={character.status === "Alive" ? "success_color" : "danger_color"}>
         {character.status}
       </h4>
       <img alt="" src={character.image}></img>
+      <button
+        className={darkMode ? "button-darkmode mt-2 mb-2 " : "button-ligth mt-2 mb-2 "}
+        onClick={() => handleClick(character)}
+      >
+        Agregar a favoritos
+      </button>
     </div>
   );
 }
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import Character from "./Character";
 
+const initialState = {
+  favorites: [],
+};
+
+const favoriteReducers = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAVORITE":
+      if (state.favorites.filter((favorite) => favorite === action.payload).length > 0) {
+        return {
+          ...state,
+          favorites: [
+            ...state.favorites.filter((favorite) => favorite !== action.payload),
+            action.payload,
+          ],
+        };
+      } else {
+        return {
+          ...state,
+          favorites: [...state.favorites, action.payload],
+        };
+      }
+
+    default:
+      return state;
+  }
+};
+
 export default function Characters({ darkMode }) {
+  const [favorites, dispatch] = useReducer(favoriteReducers, initialState);
   const [characters, setCharacters] = useState([]);
+
+  const handleClick = (favorite) => {
+    dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
+  };
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
@@ -11,10 +43,41 @@ export default function Characters({ darkMode }) {
   }, []);
 
   return (
-    <div className="characters">
-      {characters.map((character) => {
-        return <Character darkMode={darkMode} character={character}></Character>;
-      })}
-    </div>
+    <>
+      {favorites.favorites.length > 0 && (
+        <>
+          <h2 className={`${darkMode && "text-ligth"} m-2`}>Favorite characters</h2>
+          <div className="characters">
+            {favorites &&
+              favorites.favorites.length > 0 &&
+              favorites.favorites.map((character) => {
+                return (
+                  <Character
+                    key={character.id}
+                    favorites={favorites}
+                    darkMode={darkMode}
+                    handleClick={() => {}}
+                    character={character}
+                  ></Character>
+                );
+              })}
+          </div>
+        </>
+      )}
+      <h2 className={`${darkMode && "text-ligth"} m-2`}>Characters</h2>
+      <div className="characters">
+        {characters.map((character) => {
+          return (
+            <Character
+              key={character.id}
+              favorites={favorites}
+              darkMode={darkMode}
+              handleClick={handleClick}
+              character={character}
+            ></Character>
+          );
+        })}
+      </div>
+    </>
   );
 }
